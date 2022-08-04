a7e6004bd123856bd60e8b06991a0e4e4cadda8e
npv34 <phanvanluan611996@gmail.com>
2022-08-04 14:58:28 +0700

    demo mui UI

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,59 +1,146 @@
-import {NextPage} from "next";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from '@mui/material/Alert';
 import {useState} from "react";
+import LinearProgress from '@mui/material/LinearProgress';
 import axios from "axios";
-import {useRouter} from "next/router";
 
-const Login: NextPage = () => {
-    const router = useRouter()
-    const [form, setForm] = useState({
-        data: {
-            email: '',
-            password: ''
-        },
-        error: {
-            message: ''
-        }
-    })
 
-    const handlerChange = (e: any, field: any) => {
-        // @ts-ignore
-        form.data[field] = e.target.value
-        setForm({...form})
-    }
-
-   const handlerSubmitted = (e: any) => {
-        e.preventDefault()
-        let data = form.data
-        axios.post('http://eshop-ecommert.herokuapp.com/api/login', data).then(response => {
-
-            let result = response.data;
-            console.log(result)
-
-            let token = result.authorisation.token
-            localStorage.setItem('token', JSON.stringify(token));
-            router.push('/products')
-            //setForm({...form})
-        } ).catch(err => {
-            form.error.message = err.response.data.message
-            setForm({...form})
-        })
-    }
-
+function Copyright(props: any) {
     return (
-        <>
-            <p style={{color: "red"}}>{form.error.message}</p>
-            <form onSubmit={handlerSubmitted}>
-                email:
-                <input type="text" onChange={(e) => handlerChange(e, "email")} name="email"/>
-                password:
-                <input type="text" name="password" onChange={(e) => handlerChange(e, "password")}/>
-                <button type="submit">Login</button>
-            </form>
-        </>
-    )
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
+const theme = createTheme();
 
+export default function SignIn() {
+    const [error, setError] = useState('')
 
-export default Login
+    const [progress, setProgress] = React.useState(0);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const timer = setInterval(() => {
+            setProgress((oldProgress) => {
+                if (oldProgress === 100) {
+                    return 0;
+                }
+                const diff = Math.random() * 10;
+                return Math.min(oldProgress + diff, 100);
+            });
+        }, 500);
+
+        const data = new FormData(event.currentTarget);
+
+         let info = {
+            email: data.get('email'),
+            password: data.get('password'),
+        }
+
+        axios.post('http://eshop-ecommert.herokuapp.com/api/login', info).then(r => {
+            console.log(r)
+            setProgress(100)
+            clearInterval(timer);
+        }).catch(error => {
+            let errMess = JSON.stringify(error.response.data.message)
+            setError(errMess)
+            setProgress(100)
+            clearInterval(timer);
+        })
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ width: '100%' }}>
+                <LinearProgress variant="determinate" value={progress} />
+            </Box>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        { error && <Alert severity="error">{error}</Alert>}
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
